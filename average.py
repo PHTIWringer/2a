3370116cac6f851244c8c7b24cc96f79a00578ab
Ken Hileman <ken.hileman@outlook.com>
2024-04-02 19:50:16 -0400

    assignment 2a

---
+++ average.py
@@ -0,0 +1,14 @@
+# Author: Kenneth Hileman
+# GitHub username: PHTIWringer
+# Date: 04/02/2024
+# Description: Asks the user for five numbers and then prints the average.
+
+print("Please enter five numbers.")
+num_1 = float(input())
+num_2 = float(input())
+num_3 = float(input())
+num_4 = float(input())
+num_5 = float(input())
+
+print("The average of those numbers is:",
+      (num_1 + num_2 + num_3 + num_4 + num_5) / 5)
